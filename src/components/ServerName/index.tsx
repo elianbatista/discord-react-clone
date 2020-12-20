5413a303f5d33b20c34658e56e10b7720e87c7a1
Elian Batista <elianbatista2000@gmail.com>
2020-12-20 13:14:44 -0300

    ServerName

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Container, Title, ExpandIcon } from './styles'
+
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <Title>
+        Servidor do Elian
+      </Title>
+      <ExpandIcon />
+    </Container>
+  )
+}
+
+export default ServerName
